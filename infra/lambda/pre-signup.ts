4bc77d4059fd47b18c4adeb7ecd82fb0ffda54c0
Satchit Seth <satchit.seth30@gmail.com>
2026-03-19 03:39:11 -0400

    hamdled aws cognito auth

--- infra/lambda/pre-signup.ts
+++ infra/lambda/pre-signup.ts
@@ -1,11 +1,5 @@
 // @ts-nocheck
-/** @param {any} event */
 exports.handler = async (event) => {
-  const email = String(event?.request?.userAttributes?.email ?? "").toLowerCase();
-  if (!email.endsWith("@gatech.edu")) {
-    throw new Error("Only @gatech.edu email addresses are accepted.");
-  }
-
   event.response = event.response ?? {};
   event.response.autoConfirmUser = true;
   event.response.autoVerifyEmail = true;
