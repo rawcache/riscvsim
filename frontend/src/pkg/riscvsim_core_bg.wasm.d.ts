76ddbb36ea3be3b88eefa05274babaae2d949b82
Satchit Seth <satchit.seth30@gmail.com>
2026-03-20 04:45:11 -0400

    build: commit pre-built wasm pkg to skip rust compile on amplify

---
+++ frontend/src/pkg/riscvsim_core_bg.wasm.d.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_simulator_free: (a: number, b: number) => void;
+export const simulator_get_register: (a: number, b: number) => number;
+export const simulator_get_registers: (a: number) => [number, number];
+export const simulator_halted: (a: number) => number;
+export const simulator_load_data: (a: number, b: number, c: number, d: number) => [number, number];
+export const simulator_load_program: (a: number, b: number, c: number) => [number, number];
+export const simulator_new: (a: number) => number;
+export const simulator_pc: (a: number) => number;
+export const simulator_read_memory: (a: number, b: number, c: number) => [number, number];
+export const simulator_reset: (a: number) => void;
+export const simulator_step: (a: number) => [number, number, number];
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
